907d56fb172852f63b6577616c49cf490b0891c5
JamilaBayramova <willowsandersen@gmail.com>
2025-05-15 03:07:32 +0400

    Update server.py for Render dynamic PORT and all interfaces binding

--- movie_app/server.py
+++ movie_app/server.py
@@ -1,9 +1,10 @@
 import socket
 import threading
+import os
 from client_handler import handle_client
 
-HOST = '127.0.0.1'
-PORT = 5000
+HOST = '0.0.0.0'  # Bind to all interfaces
+PORT = int(os.getenv('PORT', 5000))  # Use Render's PORT env or default to 5000
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
